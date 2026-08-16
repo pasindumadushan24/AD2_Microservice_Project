34b0356e114033fc566f1dbe121ac5ce8b4592ae
Pasindu Madushan <pasindu24hh@gmail.com>
2026-08-16 15:23:21 +0530

    Controlle Change

--- parking-service/src/main/java/lk/ijse/ad2/parking/controller/ParkingController.java
+++ parking-service/src/main/java/lk/ijse/ad2/parking/controller/ParkingController.java
@@ -24,6 +24,7 @@ public class ParkingController {
     @Autowired
     private ParkingSpaceRepository parkingSpaceRepository;
 
+
     @Autowired
     private ReservationRepository reservationRepository;
 
