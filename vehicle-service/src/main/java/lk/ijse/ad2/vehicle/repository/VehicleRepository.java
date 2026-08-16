207590f886816adc426f023724da7162187ced53
Pasindu Madushan <pasindu24hh@gmail.com>
2026-08-16 17:31:11 +0530

    Update

--- vehicle-service/src/main/java/lk/ijse/ad2/vehicle/repository/VehicleRepository.java
+++ vehicle-service/src/main/java/lk/ijse/ad2/vehicle/repository/VehicleRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
+
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
     Optional<Vehicle> findByLicensePlate(String licensePlate);
     List<Vehicle> findByUserId(Long userId);
