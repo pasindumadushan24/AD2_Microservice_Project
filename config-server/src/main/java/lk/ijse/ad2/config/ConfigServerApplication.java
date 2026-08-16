4a9aa5e40212017513918981cc85f9f675449259
Pasindu Madushan <pasindu24hh@gmail.com>
2026-08-16 15:43:49 +0530

    Change

--- config-server/src/main/java/lk/ijse/ad2/config/ConfigServerApplication.java
+++ config-server/src/main/java/lk/ijse/ad2/config/ConfigServerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
+
 public class ConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
