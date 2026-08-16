73ae7b2d39af733066ab3024b9359431d0cbddaa
Pasindu Madushan <pasindu24hh@gmail.com>
2026-08-16 15:44:24 +0530

    Service

--- parking-service/src/main/java/lk/ijse/ad2/parking/ParkingServiceApplication.java
+++ parking-service/src/main/java/lk/ijse/ad2/parking/ParkingServiceApplication.java
@@ -14,6 +14,7 @@ public class ParkingServiceApplication {
         SpringApplication.run(ParkingServiceApplication.class, args);
     }
 
+
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
