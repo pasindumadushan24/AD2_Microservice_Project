e2b367f79a001d1634b9c409aa13c41f3e3f7042
Pasindu Madushan <pasindu24hh@gmail.com>
2026-08-16 15:21:48 +0530

    User

--- user-service/src/main/java/lk/ijse/ad2/user/model/User.java
+++ user-service/src/main/java/lk/ijse/ad2/user/model/User.java
@@ -40,6 +40,7 @@ public class User {
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
 
+
     public String getRole() { return role; }
     public void setRole(String role) { this.role = role; }
 }
