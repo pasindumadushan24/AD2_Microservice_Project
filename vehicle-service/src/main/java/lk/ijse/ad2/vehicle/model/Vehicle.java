4baa3358be9269665bf94759a52fa6c06cdac045
Pasindu Madushan <pasindu24hh@gmail.com>
2026-08-16 17:30:49 +0530

    Update

--- vehicle-service/src/main/java/lk/ijse/ad2/vehicle/model/Vehicle.java
+++ vehicle-service/src/main/java/lk/ijse/ad2/vehicle/model/Vehicle.java
@@ -45,6 +45,7 @@ public class Vehicle {
     public Long getUserId() { return userId; }
     public void setUserId(Long userId) { this.userId = userId; }
 
+
     public String getStatus() { return status; }
     public void setStatus(String status) { this.status = status; }
 }
